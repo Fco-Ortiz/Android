7172de0b85d633267138f490a0a4ffbff2a6a798
Zitro11 <israelortiz2001@gmail.com>
2024-05-16 23:39:38 -0500

    Metodo EDITAR

--- index.js
+++ index.js
@@ -27,18 +27,24 @@ app.get('/peliculas', async (req, res) => {
         })
         res.status(200).json(peliculas);
     } catch (error) {
-        res.status(400).send(`Error al consultar peliculas: ${error}`)
+        res.status(400).send(`Error al consultar peliculas: ${error}`);
     }
 });
 
-//Get titulo
-app.get('/peliculas/:titulo', (req, res) => {
-    const titulo = req.params.titulo;
-    const pelicula = peliculas.find(p => p.titulo === titulo);
-    if(!peliculas){
-        return res.status(404).json({ mensaje: 'Pelicula no encontrada' });
+//Consultar:Titulo (GET:Titulo -> FireStore)*
+app.get('/peliculas/:Titulo', async (req, res) => {
+    try {
+        const titulo = (req.params.Titulo).toLowerCase().replace(/\s+/g, '');//Minusculas y sin espacios
+        const snapshot = await db.collection('Pelicula').where('Titulo2', '==', titulo).get();
+        if(snapshot.empty){res.status(404).send('No se encontraron Peliculas'); return;}    //validamos si esta vacia la consulta
+        let pelicula = [];
+        snapshot.forEach(doc => {
+            pelicula.push({ id: doc.id, ...doc.data() });
+        })
+        res.status(200).json(pelicula);
+    } catch (error) {
+        res.status(400).send(`Error al buscar la pelicula: ${error}`)
     }
-    res.json(pelicula);
 });
 
 //Agregar (Post -> FireStore)*
@@ -46,22 +52,26 @@ app.post('/peliculas', async (req,res) => {
     try {
         const pelicula = req.body;  //Obtenemos los datos del cuerpo del json
         const docFire = await db.collection('Pelicula').add(pelicula);  //Agregamos la plicula
-        res.status(201).send(`Pelicula agregada: ${docFire.titulo}`);   //Regresamos la pelicula agregada
+        res.status(201).send(`Pelicula agregada: ${docFire.Titulo1}`);   //Regresamos la pelicula agregada
     } catch (error) {
         res.status(400).send(`Error al agregar pelicula: ${error}`) //Manejo de error        
     }
 })
 
-//Editar
-app.put('/peliculas/:titulo', (req, res) => {
-    const titulo = req.params.titulo;
-    const indice = peliculas.findIndex(p => p.titulo === titulo);
-    if (indice === -1) {
-        return res.status(404).json({ mensaje: 'Película no encontrada' });
+//Editar (PUT -> FireStore)*
+app.put('/peliculas/:Titulo', async (req, res) => {
+    try {
+        const titulo = (req.params.Titulo).toLowerCase().replace(/\s+/g, '');//Minusculas y sin espacios
+        const newData = req.body;   //Guardamos los nuevos datos
+        const snapshot = await db.collection('Pelicula').where('Titulo2', '==', titulo).get();  //Contiene los doc que cumplen el criterio
+        if(snapshot.empty){res.status(404).send('No se encontro la Pelicula'); return;}    //validamos si esta vacia la consulta
+        //Actualizar los datos
+        const doc = snapshot.docs[0];
+        await doc.ref.update(newData);
+        res.status(200).send(`La pelicula "${titulo}", se actualizo`);
+    } catch (error) {
+        res.status(500).send(`Error al intentar actualizar: ${error}`);
     }
-    const nuevaPelicula = req.body;
-    peliculas[indice] = nuevaPelicula;
-    res.status(200).json({ mensaje: 'Película actualizada correctamente' });
 });
 
 //Eliminar
