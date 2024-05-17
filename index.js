54a1c6a1bcb2fec95c94578f8f5dcbeb2249bc8d
Zitro11 <israelortiz2001@gmail.com>
2024-05-16 23:49:50 -0500

    Metodo DELETE

--- index.js
+++ index.js
@@ -66,8 +66,7 @@ app.put('/peliculas/:Titulo', async (req, res) => {
         const snapshot = await db.collection('Pelicula').where('Titulo2', '==', titulo).get();  //Contiene los doc que cumplen el criterio
         if(snapshot.empty){res.status(404).send('No se encontro la Pelicula'); return;}    //validamos si esta vacia la consulta
         //Actualizar los datos
-        const doc = snapshot.docs[0];
-        await doc.ref.update(newData);
+        await snapshot.docs[0].ref.update(newData); //Docs[0] trae el primer documento de la consulta (siempre debe de haber solo 1)
         res.status(200).send(`La pelicula "${titulo}", se actualizo`);
     } catch (error) {
         res.status(500).send(`Error al intentar actualizar: ${error}`);
@@ -75,14 +74,16 @@ app.put('/peliculas/:Titulo', async (req, res) => {
 });
 
 //Eliminar
-app.delete('/peliculas/:titulo', (req,res) => {
-    const titulo = req.params.titulo;
-    const indice = peliculas.findIndex(p => p.titulo === titulo);
-    if (indice === -1) {
-        return res.status(404).json({ mensaje: 'Película no encontrada' });
+app.delete('/peliculas/:Titulo', async (req,res) => {
+    try {
+        const titulo = (req.params.Titulo).toLowerCase().replace(/\s+/g, '');//Minusculas y sin espacios
+        const snapshot = await db.collection('Pelicula').where('Titulo2', '==', titulo).get();
+        if(snapshot.empty){res.status(404).send('No se encontraron Peliculas'); return;}   //validamos si esta vacia la consulta
+        await snapshot.docs[0].ref.delete();    //Eliminamos la pelicula
+        res.status(200).send(`Pelicula: ${titulo}, se elimino correctamente`);
+    } catch (error) {
+        res.status(500).send(`Error al intentar borrar la pelicula: ${error}`)       
     }
-    peliculas.splice(indice, 1);
-    res.status(200).json({ mensaje: 'Película eliminada correctamente' });
 })
 
 //Iniciar el servidor
